c3b5baee273a526a852558dba80c8051ae73d8e4
祝一丹 <zhuyidann@zhuyidiandeMacBook-Pro.local>
2025-11-05 00:25:47 +0800

    添加base配置以支持GitHub Pages部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 
-  import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
   export default defineConfig({
+    base: './',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
